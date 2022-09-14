90d0fca51c5373b81ffcc77cd002ee44f212754e
chiberry <96498350+chiberry@users.noreply.github.com>
2022-09-13 22:46:57 -0400

    Update EC2-main.tf

--- EC2-main.tf
+++ EC2-main.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "OMS-EC2-Webserver-SG" {
 
 # Resource-8: Creat EC2 Ubuntu  Web Server
 resource "aws_instance" "OMS-EC2-Webserver" {
-  ami           = "ami-0568773882d492fc8"
+  ami           = "ami-HERE"
   instance_type = "t2.micro"
   key_name      = "xxx"
   subnet_id     = aws_subnet.OMS-VPC-Pub-sbn.id
@@ -56,4 +56,29 @@ resource "aws_instance" "OMS-EC2-Webserver" {
     Name = "OMS-EC2-Webserver"
   }
 }
+# Resource-8: Creat EC2 Ubuntu  Web Server
+resource "aws_instance" "OMS-EC2-Webserver" {
+  ami           = "ami-HERE"
+  instance_type = "t2.micro"
+  key_name      = "KEY-HERE"
+  subnet_id     = aws_subnet.OMS-VPC-Pub-sbn.id
+  vpc_security_group_ids = [aws_security_group.OMS-EC2-Webserver-SG.id]
+  count                     = "1"
+  user_data = <<-EOF
+<<-EOF
+              #!/bin/bash
+              sudo apt-get install nginx -y
+              echo "Welcome to Patara Web Application Server" > /var/www/html/index.html
+              sudo apt-get update -y
+              sudo service httpd start
+              
+              sudo curl -fsSL https://get.docker.com -o get-docker.sh
+              sudo sh get-docker.sh  
+                                          
+              EOF
 
+
+  tags = {
+    Name = "OMS-EC2-Webserver"
+  }
+}
